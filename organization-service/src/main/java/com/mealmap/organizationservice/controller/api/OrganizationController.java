27c76a93e62e007c4ed293757ffb456e7a57eedf
danilalisichkin <danilafox3467346@gmail.com>
2025-03-13 15:52:23 +0300

    feat(organization-service): implement API layer

--- organization-service/src/main/java/com/mealmap/organizationservice/controller/api/OrganizationController.java
+++ organization-service/src/main/java/com/mealmap/organizationservice/controller/api/OrganizationController.java
@@ -6,20 +6,32 @@ import com.mealmap.organizationservice.core.dto.organization.OrganizationDto;
 import com.mealmap.organizationservice.core.dto.organization.OrganizationUpdatingDto;
 import com.mealmap.organizationservice.core.dto.page.PageDto;
 import com.mealmap.organizationservice.core.enums.sort.OrganizationSortField;
+import com.mealmap.organizationservice.service.OrganizationService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Validated
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/organizations")
 public class OrganizationController {
+    private final OrganizationService organizationService;
 
     @GetMapping
     public ResponseEntity<PageDto<OrganizationDto>> getPageOfOrganizations(
@@ -29,26 +41,34 @@ public class OrganizationController {
             @RequestParam(defaultValue = "ASC") Sort.Direction sortOrder,
             @ModelAttribute @Valid OrganizationFilterDto filter) {
 
-        return ResponseEntity.status(HttpStatus.OK).build();
+        PageDto<OrganizationDto> page =
+                organizationService.getPageOfOrganizations(offset, limit, sortBy, sortOrder, filter);
+
+        return ResponseEntity.status(HttpStatus.OK).body(page);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<OrganizationDto> getOrganization(@PathVariable Integer id) {
+        OrganizationDto organization = organizationService.getOrganization(id);
 
-        return ResponseEntity.status(HttpStatus.OK).build();
+        return ResponseEntity.status(HttpStatus.OK).body(organization);
     }
 
     @PostMapping
     public ResponseEntity<OrganizationDto> createOrganization(
             @RequestBody @Valid OrganizationCreationDto organizationDto) {
 
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        OrganizationDto organization = organizationService.createOrganization(organizationDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(organization);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<OrganizationDto> updateOrganization(
             @PathVariable Integer id, @RequestBody @Valid OrganizationUpdatingDto organizationDto) {
 
-        return ResponseEntity.status(HttpStatus.OK).build();
+        OrganizationDto organization = organizationService.updateOrganization(id, organizationDto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(organization);
     }
 }
