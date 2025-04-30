2c6cf071b92c862c6ccbb368d4ed0bf2af8ba57f
danilalisichkin <danilafox3467346@gmail.com>
2025-04-30 14:45:35 +0300

    feat(frontend): add OrderDetails

---
+++ frontend/src/components/features/OrderDetails/OrderDetails.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { OrderDto } from "../../../api/order/dto/OrderDto";
+
+interface OrderDetailsProps {
+  order: OrderDto;
+}
+
+const OrderDetails: React.FC<OrderDetailsProps> = ({ order }) => (
+  <div className="border-t border-gray-200 mt-6 pt-6">
+    <div className="flex justify-between">
+      <span className="text-gray-700">Сумма заказа:</span>
+      <span className="font-medium">
+        {order.paymentInfo.totalAmount + order.paymentInfo.discountAmount}₽
+      </span>
+    </div>
+    <div className="flex justify-between mt-2">
+      <span className="text-gray-700">Доставка:</span>
+      <span className="font-medium">НЕТ₽</span>
+    </div>
+    {order.paymentInfo.discountAmount > 0 && (
+      <div className="flex justify-between mt-2">
+        <span className="text-gray-700">Промокод:</span>
+        <span className="font-medium text-green-600">
+          -{order.paymentInfo.discountAmount}₽
+        </span>
+      </div>
+    )}
+    <div className="flex justify-between mt-4 pt-4 border-t border-gray-200 font-bold text-lg">
+      <span>Итого:</span>
+      <span>{order.paymentInfo.totalAmount.toLocaleString()}₽</span>
+    </div>
+  </div>
+);
+
+export default OrderDetails;
