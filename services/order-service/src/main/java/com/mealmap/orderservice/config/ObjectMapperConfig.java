bc820488ab78710afd1b0fa0571c6c1c23c7f64b
danilalisichkin <danilafox3467346@gmail.com>
2025-05-02 17:01:29 +0300

    fix(order-service): fix time serialization
    - made time serialization by ISO format

--- services/order-service/src/main/java/com/mealmap/orderservice/config/ObjectMapperConfig.java
+++ services/order-service/src/main/java/com/mealmap/orderservice/config/ObjectMapperConfig.java
@@ -1,13 +1,20 @@
 package com.mealmap.orderservice.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.mealmap.orderservice.core.serialization.ObjectIdDeserializer;
 import com.mealmap.orderservice.core.serialization.ObjectIdSerializer;
 import org.bson.types.ObjectId;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class ObjectMapperConfig {
@@ -20,7 +27,12 @@ public class ObjectMapperConfig {
         module.addDeserializer(ObjectId.class, new ObjectIdDeserializer());
         mapper.registerModule(module);
 
-        mapper.registerModule(new JavaTimeModule());
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ISO_LOCAL_DATE));
+
+        mapper.registerModule(javaTimeModule);
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
         return mapper;
     }
