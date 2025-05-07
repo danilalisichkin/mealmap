acd4c738e7d888792e92e96647bdc4705ef27647
danilalisichkin <danilafox3467346@gmail.com>
2025-05-07 19:58:36 +0300

    feat(file-service): integrate with Eureka Server

--- services/file-service/src/main/java/com/example/fileservice/FileServiceApplication.java
+++ services/file-service/src/main/java/com/example/fileservice/FileServiceApplication.java
@@ -3,8 +3,10 @@ package com.example.fileservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ConfigurationPropertiesScan
 public class FileServiceApplication {
 
