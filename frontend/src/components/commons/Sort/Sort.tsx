014de9900174b0b5604a46f0c3b73b81c12b4ddc
danilalisichkin <danilafox3467346@gmail.com>
2025-05-10 18:57:35 +0300

    fix(frontend): fix Sort component

--- frontend/src/components/commons/Sort/Sort.tsx
+++ frontend/src/components/commons/Sort/Sort.tsx
@@ -24,7 +24,7 @@ const Sort = <T extends string>({
   };
 
   return (
-    <div className="flex items-center justify-center gap-2 text-green-600 hover:text-green-800 border border-green-600 hover:border-green-800 px-4 py-2 rounded-lg transition-colors">
+    <div className="flex items-center justify-center gap-2 text-green-600 hover:text-green-800 border border-green-600 hover:border-green-800 px-4 py-2 md:py-0 rounded-lg transition-colors">
       <label htmlFor="sort-field">
         сортировать по:
       </label>
